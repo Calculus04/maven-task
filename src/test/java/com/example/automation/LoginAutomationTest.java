2569fdcc497ab653d2dce5bf7368b2f539c33ea7
Calculus04 <109899901+Calculus04@users.noreply.github.com>
2025-01-02 14:22:45 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -13,7 +13,7 @@ public class LoginAutomationTest {
     @Test
     public void testLogin() {
         // Set up the WebDriver
-        System.setProperty("webdriver.chrome.driver", "C:\Users\shrut\Downloads\chromedriver-win64\chromedriver-win64");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shrut\\Downloads\\chromedriver-win64\\chromedriver-win64");
         WebDriver driver = new ChromeDriver();
 
         try {
